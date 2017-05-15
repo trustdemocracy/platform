e5bf3b532824acb96208c7c79ed9d51f8dadc077
Luis Emilio Velasco Sanchez <emibloque@gmail.com>
2017-05-15 11:55:18 +0200

    Add init database scripts

---
+++ init/mysql/proposals.sql
@@ -0,0 +1,32 @@
+CREATE TABLE `proposals` (
+    `id` VARCHAR(36) NOT NULL,
+    `author` VARCHAR(100),
+    `title` VARCHAR(100),
+    `brief` VARCHAR(500),
+    `source` VARCHAR(1000),
+    `motivation` TEXT(20000),
+    `measures` TEXT(20000),
+    `status` VARCHAR(20),
+    PRIMARY KEY (id)
+)  ENGINE=INNODB DEFAULT CHARSET=UTF8 COLLATE = UTF8_UNICODE_CI;
+
+
+CREATE TABLE `comments` (
+    `id` VARCHAR(36) NOT NULL,
+    `proposal_id` VARCHAR(36) NOT NULL,
+    `root_comment_id` VARCHAR(36) NOT NULL,
+    `author_id` VARCHAR(36) NOT NULL,
+    `author_username` VARCHAR(36) NOT NULL,
+    `content` VARCHAR(3000),
+    `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+)  ENGINE=INNODB DEFAULT CHARSET=UTF8 COLLATE = UTF8_UNICODE_CI; 
+
+
+CREATE TABLE `votes` (
+    `comment_id` VARCHAR(36) NOT NULL,
+    `voter_id` VARCHAR(36) NOT NULL,
+    `value_option` VARCHAR(10) NOT NULL,
+    PRIMARY KEY (comment_id , voter_id)
+)  ENGINE=INNODB DEFAULT CHARSET=UTF8 COLLATE = UTF8_UNICODE_CI;
+
